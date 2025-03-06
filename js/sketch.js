be81df6347b914f2ac6cc52d0fa79ba98a92071e
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2025-03-06 17:51:43 +0000

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,7 @@
 var left = 0;
 var right = 0;
+var counter = 0;
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +14,7 @@ function draw() {
   fill('white')
   text(left,windowWidth/4,100)
   text(right,(windowWidth/4)+(windowWidth/2),100)
+  text(letters[counter],windowWidth/2,100)
   
 }
 
@@ -22,4 +25,5 @@ function touchEnded(){
   if (mouseX > windowWidth/2){
     right +=1;
   }
+  counter += 1;
 }
