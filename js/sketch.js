9d7c02da5812a9e1d4fd4aff09e0fa11bca9b598
jmonk603 <91668513+jmonk603@users.noreply.github.com>
2025-03-06 16:08:22 +0000

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,33 +1,31 @@
-// ICM-2016
+// On a touchscreen device, touch the canvas using one or more fingers
+// at the same time.
 
-// Take a look at the HTML file where some things have been
-// added for mobile viewing
-
-var colors;
+let value = 0;
 
 function setup() {
-  // Make the canvas the size of the mobile device screen
-  createCanvas(windowWidth, windowHeight);
-  background(200);
+  createCanvas(100, 100);
 
-  // An array of five colors, one for each finger
-  colors = [color(255,0,0), color(0,255,0), color(0,0,255), color(255, 255,0), color(0,255,255)];
+  describe(
+    'A gray square with a black square at its center. The inner square switches color between black and white each time the user stops touching the screen.'
+  );
 }
 
 function draw() {
-  // The touches array holds an object for each and every touch
-  // The array length is dynamic and tied to the number of fingers 
-  // currently touching
-  for (var i = 0; i < touches.length; i++) {
-    noStroke();
-    // One color per finger
-    fill(colors[i]);
-    // Draw a circle at each finger
-    ellipse(touches[i].x, touches[i].y, 24, 24);
-  }
+  background(200);
+
+  // Style the square.
+  fill(value);
+
+  // Draw the square.
+  square(25, 25, 50);
 }
 
-// this prevents dragging screen around
-function touchMoved() {
-  return false;
+// Toggle colors when a touch ends.
+function touchEnded() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
 }
